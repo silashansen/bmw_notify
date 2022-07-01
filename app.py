d50b0bd68aac6007dabaed3ad92884812d9cfab3
Silas Hansen <silashansen@gmail.com>
2022-07-01 10:39:50 +0200

    Updated bimmer_connected from v0.8.0 to v0.10.0

--- app.py
+++ app.py
@@ -1,9 +1,15 @@
 import bimmer_connected
+import asyncio
 from bimmer_connected.account import ConnectedDriveAccount
-from bimmer_connected.country_selector import Regions
+from bimmer_connected.account import MyBMWAccount
+from bimmer_connected.api.regions import Regions
 import os
 from twilio.rest import Client
 import json
+from dotenv import load_dotenv
+
+load_dotenv()
+
 
 twilio_account_sid = os.environ.get('TWILIO_ACCOUNT_SID')
 twilio_auth_token = os.environ.get('TWILIO_AUTH_TOKEN')
@@ -12,7 +18,7 @@ receivers = os.environ.get('SMS_RECEIVERS').split(",")
 login = {
             'username': os.environ.get('BMW_USERNAME'), 
             'password': os.environ.get('BMW_PASSWORD'),
-            "regions": Regions(2)
+            "regions": Regions.REST_OF_WORLD
         }
 
 def notify_lock_state(vehicle):
@@ -30,18 +36,21 @@ def send_sms(message, number):
          to=number
      )
 
-def handler(event, context):
+async def handler(event, context):
     
     sendSuccessNotification = os.environ.get('SEND_SUCCESS_NOTIFICATION')
 
     try:
         print("Connecting to BMW Connected")
-        account = ConnectedDriveAccount(login["username"], login["password"], login["regions"])
+
+        account = MyBMWAccount(login["username"], login["password"], login["regions"])
+        await account.get_vehicles()
+
         print(f"Fetched {str(len(account.vehicles))} vehicles")
 
         for vehicle in account.vehicles:
             print(f"Vehicle {vehicle.name} is {vehicle.status.door_lock_state}")
-            if vehicle.status.door_lock_state != "LOCKED" or sendSuccessNotification == "true":
+            if vehicle.doors_and_windows.door_lock_state != "SECURED" or sendSuccessNotification == "true":
                 notify_lock_state(vehicle)
 
         return {
@@ -55,4 +64,4 @@ def handler(event, context):
         }
 
 if __name__ == "__main__":
-    handler(None, None)
+    asyncio.run(handler(None, None))
